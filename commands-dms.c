1c39b551f3b74d342238d7ae1b4921f7c1e41d94
Felix Fietkau <nbd@openwrt.org>
2013-06-24 00:44:27 +0200

    add --reset-dms

--- commands-dms.c
+++ commands-dms.c
@@ -90,3 +90,11 @@ cmd_dms_get_imsi_prepare(struct qmi_dev *qmi, struct qmi_request *req, struct qm
 	qmi_set_dms_uim_get_imsi_request(msg);
 	return QMI_CMD_REQUEST;
 }
+
+#define cmd_dms_reset_cb no_cb
+static enum qmi_cmd_result
+cmd_dms_reset_prepare(struct qmi_dev *qmi, struct qmi_request *req, struct qmi_msg *msg, char *arg)
+{
+	qmi_set_dms_reset_request(msg);
+	return QMI_CMD_REQUEST;
+}
