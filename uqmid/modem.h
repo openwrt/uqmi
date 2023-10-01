5469442fcfbbeccafee93f66cbc60ffe66365b83
Alexander Couzens <lynxis@fe80.eu>
2023-10-01 17:38:49 +0200

    add uqmid: a daemonic version of uqmi
    It is configured via ubus to handle QMI based modem and keep
    the state of the modem in difference to uqmi as command utility.
    Further it also allows to react on events (indications) of the modem.
    See uqmid/examples/ for more usage.
    
    Signed-off-by: Alexander Couzens <lynxis@fe80.eu>

---
+++ uqmid/modem.h
@@ -0,0 +1,123 @@
+/*
+ * uqmi -- tiny QMI support implementation
+ *
+ * Copyright (C) 2023 Alexander Couzens <lynxis@fe80.eu>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __UQMID_MODEM_H
+#define __UQMID_MODEM_H
+
+#include "sim.h"
+#include <libubus.h>
+#include <netinet/in.h>
+
+#define IMEI_LEN 15
+#define PATH_LEN 128
+
+
+// try to get osmocom fsm into here?
+struct modem_config {
+	bool configured;
+	char *apn;
+	char *pin;
+	bool roaming;
+	uint8_t pdp_type;
+};
+
+struct modem {
+	char *name;
+	char *device;
+	char imei[IMEI_LEN];
+	char path[PATH_LEN];
+
+	char *manuf;
+	char *model;
+	char *rev;
+
+	/* unsure if iccid should be here */
+	char *iccid;
+	char *imsi;
+
+	struct modem_config config;
+	struct {
+		/* Radio Access Technology */
+		int rat;
+		/* Mobile Country Code */
+		int mcc;
+		/* Mobile Network Code */
+		int mnc;
+		/* len can be 2 or 3 */
+		int mnc_len;
+		/* Operator name, can be from Sim or network. */
+		char *operator_name;
+		/* attached to Circuit Switch/Voice */
+		bool cs;
+		/* attached to Packet Switch/Data */
+		bool ps;
+		/* if an error happened and the modem should stay off */
+		char *error;
+	} state;
+
+	/* TODO: add multiple bearer support later */
+	struct {
+		/* The QMI internal handle */
+		uint32_t packet_data_handle;
+		/* either ipv4, ipv6, ipv46 */
+		int pdp_type;
+		/* valid v4_addr and ipv46 */
+		struct sockaddr_in v4_addr;
+		struct sockaddr_in v4_netmask;
+		struct sockaddr_in v4_gateway;
+		/* valid v6 and ipv46 */
+		struct sockaddr_in6 v6;
+		struct sockaddr_storage dns1;
+		struct sockaddr_storage dns2;
+	} brearer;
+
+	struct {
+		/* Do we found a valid simcard */
+		bool init;
+		/* Certain modems (and maybe simcards) support either unlocking the sim via pin1 or upin. */
+		enum uqmi_sim_state state;
+		bool use_upin;
+		bool requires_unlock;
+		int pin_retries;
+		int puk_retries;
+	} sim;
+
+	struct qmi_dev *qmi;
+
+	struct list_head list;
+	struct ubus_object ubus;
+
+	struct osmo_fsm_inst *fi;
+};
+
+int uqmid_modem_add(const char *name, const char *device, bool qmi_over_mbim);
+int uqmid_modem_remove(struct modem *modem);
+int uqmid_modem_start(struct modem *modem);
+int uqmid_modem_configured(struct modem *modem);
+void uqmid_modem_set_error(struct modem *modem, const char *error);
+
+typedef void (*uqmid_modem_get_opmode_cb)(void *data, int opmode_err, int opmode);
+int uqmid_modem_get_opmode(struct modem *modem, uqmid_modem_get_opmode_cb cb, void *cb_data);
+int uqmid_modem_networkstatus(struct modem *modem);
+struct modem *uqmid_modem_find_by_device(const char *device);
+struct modem *uqmid_modem_find_by_name(const char *name);
+
+#endif /* __UQMID_MODEM_H */
---
+++ uqmid/modem_tx.c
@@ -0,0 +1,180 @@
+/*
+ * uqmi -- tiny QMI support implementation
+ *
+ * Copyright (C) 2023 Alexander Couzens <lynxis@fe80.eu>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ */
+
+/* Used by uqmid to contain the modem state */
+
+#include <stdint.h>
+#include <talloc.h>
+
+#include "qmi-struct.h"
+#include "qmi-enums.h"
+#include "qmi-message.h"
+#include "qmi-enums-uim.h"
+
+#include "logging.h"
+#include "utils.h"
+
+#include "modem.h"
+#include "services.h"
+#include "modem_fsm.h"
+
+#include "modem_tx.h"
+
+int tx_dms_set_operating_mode(struct modem *modem, struct qmi_service *dms, uint8_t operating_mode, request_cb cb)
+{
+	struct qmi_request *req = talloc_zero(dms, struct qmi_request);
+	struct qmi_msg *msg = talloc_zero_size(req, 1024);
+
+	struct qmi_dms_set_operating_mode_request set_op_mode_req = { QMI_INIT(mode, operating_mode) };
+
+	int ret = qmi_set_dms_set_operating_mode_request(msg, &set_op_mode_req);
+	if (ret) {
+		LOG_ERROR("Failed to encode get version info");
+		return 1;
+	}
+
+	req->msg = msg;
+	req->cb = cb;
+	req->cb_data = modem;
+	return uqmi_service_send_msg(dms, req);
+}
+
+int tx_nas_subscribe_nas_events(struct modem *modem, struct qmi_service *nas, bool action, request_cb cb)
+{
+	struct qmi_request *req = talloc_zero(nas, struct qmi_request);
+	struct qmi_msg *msg = talloc_zero_size(req, 1024);
+
+	struct qmi_nas_register_indications_request register_req = {};
+	qmi_set(&register_req, serving_system_events, action);
+	qmi_set(&register_req, subscription_info, action);
+	qmi_set(&register_req, system_info, action);
+	qmi_set(&register_req, signal_info, action);
+	register_req.set.network_reject_information = 1;
+	register_req.data.network_reject_information.enable_network_reject_indications = action;
+
+	int ret = qmi_set_nas_register_indications_request(msg, &register_req);
+	if (ret) {
+		LOG_ERROR("Failed to encode get version info");
+		return 1;
+	}
+
+	req->msg = msg;
+	req->cb = cb;
+	req->cb_data = modem;
+	return uqmi_service_send_msg(nas, req);
+}
+
+int tx_wds_get_profile_list(struct modem *modem, struct qmi_service *wds, request_cb cb)
+{
+	struct qmi_request *req = talloc_zero(wds, struct qmi_request);
+	struct qmi_msg *msg = talloc_zero_size(req, 1024);
+
+	struct qmi_wds_get_profile_list_request profile_req = { QMI_INIT(profile_type, QMI_WDS_PROFILE_TYPE_3GPP) };
+
+	int ret = qmi_set_wds_get_profile_list_request(msg, &profile_req);
+	if (ret) {
+		LOG_ERROR("Failed to encode get profile list");
+		return 1;
+	}
+
+	req->msg = msg;
+	req->cb = cb;
+	req->cb_data = modem;
+	return uqmi_service_send_msg(wds, req);
+}
+
+int tx_wds_modify_profile(struct modem *modem, struct qmi_service *wds, request_cb cb, uint8_t profile, const char *apn,
+			  uint8_t pdp_type)
+{
+	struct qmi_request *req = talloc_zero(wds, struct qmi_request);
+	struct qmi_msg *msg = talloc_zero_size(req, 1024);
+
+	struct qmi_wds_modify_profile_request profile_req = {};
+
+	profile_req.set.profile_identifier = 1;
+	profile_req.data.profile_identifier.profile_type = QMI_WDS_PROFILE_TYPE_3GPP;
+	profile_req.data.profile_identifier.profile_index = profile;
+	qmi_set(&profile_req, pdp_type, pdp_type);
+
+	if (apn)
+		profile_req.data.apn_name = (char *) apn;
+
+	int ret = qmi_set_wds_modify_profile_request(msg, &profile_req);
+	if (ret) {
+		LOG_ERROR("Failed to encode get profile list");
+		return 1;
+	}
+
+	req->msg = msg;
+	req->cb = cb;
+	req->cb_data = modem;
+	return uqmi_service_send_msg(wds, req);
+}
+
+int tx_wds_start_network(struct modem *modem, struct qmi_service *wds, request_cb cb, uint8_t profile_idx,
+			 uint8_t ip_family)
+{
+	struct qmi_request *req = talloc_zero(wds, struct qmi_request);
+	struct qmi_msg *msg = talloc_zero_size(req, 1024);
+
+	struct qmi_wds_start_network_request start_req = {};
+	qmi_set(&start_req, profile_index_3gpp, profile_idx);
+	qmi_set(&start_req, ip_family_preference, ip_family);
+
+	int ret = qmi_set_wds_start_network_request(msg, &start_req);
+	if (ret) {
+		LOG_ERROR("Failed to encode start network request");
+		return 1;
+	}
+
+	req->msg = msg;
+	req->cb = cb;
+	req->cb_data = modem;
+	return uqmi_service_send_msg(wds, req);
+}
+
+int tx_wds_get_current_settings(struct modem *modem, struct qmi_service *wds, request_cb cb)
+{
+	struct qmi_request *req = talloc_zero(wds, struct qmi_request);
+	struct qmi_msg *msg = talloc_zero_size(req, 1024);
+
+	struct qmi_wds_get_current_settings_request get_settings_req = {};
+	qmi_set(&get_settings_req, requested_settings,
+		QMI_WDS_GET_CURRENT_SETTINGS_REQUESTED_SETTINGS_PDP_TYPE |
+		QMI_WDS_GET_CURRENT_SETTINGS_REQUESTED_SETTINGS_DNS_ADDRESS |
+		QMI_WDS_GET_CURRENT_SETTINGS_REQUESTED_SETTINGS_GRANTED_QOS |
+		QMI_WDS_GET_CURRENT_SETTINGS_REQUESTED_SETTINGS_IP_ADDRESS |
+		QMI_WDS_GET_CURRENT_SETTINGS_REQUESTED_SETTINGS_GATEWAY_INFO |
+		QMI_WDS_GET_CURRENT_SETTINGS_REQUESTED_SETTINGS_MTU |
+		QMI_WDS_GET_CURRENT_SETTINGS_REQUESTED_SETTINGS_DOMAIN_NAME_LIST |
+		QMI_WDS_GET_CURRENT_SETTINGS_REQUESTED_SETTINGS_IP_FAMILY);
+
+	int ret = qmi_set_wds_get_current_settings_request(msg, &get_settings_req);
+	if (ret) {
+		LOG_ERROR("Failed to encode start network request");
+		return 1;
+	}
+
+	req->msg = msg;
+	req->cb = cb;
+	req->cb_data = modem;
+	return uqmi_service_send_msg(wds, req);
+}
