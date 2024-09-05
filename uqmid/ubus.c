9ecb57ee2c1ffcc436eb5c463196294e23f1d54a
Arne Zachlod <arne@nerdkeller.org>
2024-09-05 15:48:47 +0200

    uqmid: ubus call dump: add roaming variable
    Signed-off-by: Arne Zachlod <arne@nerdkeller.org>

--- uqmid/ubus.c
+++ uqmid/ubus.c
@@ -423,6 +423,7 @@ static int modem_dump_state(struct ubus_context *ctx, struct ubus_object *obj, s
 	BLOBMSG_ADD_STR_CHECK(&b, "iccid", modem->iccid);
 	/* session state */
 	BLOBMSG_ADD_STR_CHECK(&b, "config_apn", modem->config.apn);
+	blobmsg_add_u8(&b, "roaming", modem->config.roaming);
 	blob_add_addr(&b, "ipv4_addr", (struct sockaddr *)&modem->brearer.v4_addr);
 	blob_add_addr(&b, "ipv4_netmask", (struct sockaddr *)&modem->brearer.v4_netmask);
 	blob_add_addr(&b, "ipv4_gateway", (struct sockaddr *)&modem->brearer.v4_gateway);
