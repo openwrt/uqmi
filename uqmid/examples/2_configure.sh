85e84ada615bcf991f9c65a11454e802c825b499
Alexander Couzens <lynxis@fe80.eu>
2024-09-08 15:46:10 +0200

    uqmid: examples: enable roaming
    Signed-off-by: Alexander Couzens <lynxis@fe80.eu>

--- uqmid/examples/2_configure.sh
+++ uqmid/examples/2_configure.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-CONFIG='{"apn":"internet.telekom","roaming":"true", "username": "telekom", "password": "tm"}'
+CONFIG='{"apn":"internet.telekom","roaming":"true", "username": "telekom", "password": "tm", "roaming": true }'
 ubus call uqmid.modem.modem1 configure "$CONFIG"
